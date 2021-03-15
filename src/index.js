a43143910357cc82ccdafa894dd4f60e05709dd7
fdaniil <frolov.dan.mail@gmail.com>
2021-03-15 08:53:31 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+    str = str.split("");
+    for (let i = 0; i < str.length; i++) {
+        let element = str[i];
+        let arr = bracketsConfig.find(a => a.find(x => x == element))
+        if (!arr) {
+            return false;
+        }
+        if (arr[1] == element && stack[stack.length - 1] == arr[0]) {
+            stack.pop();
+        }  else if (arr[0] == element) {
+            stack.push(element);
+        }  else {
+            return false;
+        }
+
+    }
+
+    return stack.length == 0;
 }
